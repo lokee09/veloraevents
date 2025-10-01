62c196defb00e39d9824e4ec1e9c9d3847f56765
LOKESH YADAV <Sanneboina77@gmail.com>
2025-10-01 04:59:49 +0000

    how to setup environment variable of firebase

--- src/lib/firebase/config.ts
+++ src/lib/firebase/config.ts
@@ -9,12 +9,12 @@ import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyD31UtyCjIO3Y-RjYxYBiN_I_jNQ2WD7Gs",
-  authDomain: "studio-4508634926-3fd6e.firebaseapp.com",
-  projectId: "studio-4508634926-3fd6e",
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
   storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: "159101215057",
-  appId: "1:159101215057:web:58fca6ea2b290b865bf370",
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
