997f4a640fce6088daa0c24011e72a1ada0cbe8a
LOKESH YADAV <Sanneboina77@gmail.com>
2025-10-01 05:50:08 +0000

    Error occurred prerendering page "/login". Read more: https://nextjs.org

--- C:/Users/Lokesh/projects/next.config.ts
+++ C:/Users/Lokesh/projects/next.config.ts
@@ -1,3 +1,4 @@
+import 'dotenv/config';
 import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
@@ -23,8 +24,14 @@ const nextConfig: NextConfig = {
         pathname: '/**',
       },
       {
-        protocol: 'https://',
+        protocol: 'https',
         hostname: 'picsum.photos',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'i.imgur.com',
         port: '',
         pathname: '/**',
       }
